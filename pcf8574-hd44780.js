f6fc1982fdaf563671d0071857c4422e480b0ad1
Catethysis <catethysis@gmail.com>
2018-08-30 02:51:06 +0300

    transformed to class

--- pcf8574-hd44780.js
+++ pcf8574-hd44780.js
@@ -1,61 +1,64 @@
 const i2c = require('i2c-bus');
-let i2c1, pcf_addr;
- 
-pcf8574_byte_out = (data) => {
-    i2c1.sendByteSync(pcf_addr, data);
-}
+let I2C, pcf_addr;
 
-sendCommand = (cmd) => {
-	cmd |= 0x08; pcf8574_byte_out(cmd);
-	cmd |= 0x04; pcf8574_byte_out(cmd);
-	cmd &= 0xFB; pcf8574_byte_out(cmd);
-}
+module.exports = class pcf8574_hd44780 {
+    constructor (i2c_module, addr) {
+        I2C = i2c.openSync(i2c_module);
+        pcf_addr = addr;
+    }
 
-init = (i2c_module, addr) => {
-    i2c1 = i2c.openSync(i2c_module);
-    pcf_addr = addr;
-    const initString = [0x30, 0x30, 0x30, 0x20, 0x20, 0x80, 0x00, 0x80, 0x00, 0x10, 0x00, 0x60, 0x00, 0xC0];
-    initString.forEach(cmd => sendCommand(cmd));
-}
+    byte_out (data) {
+        I2C.sendByteSync(pcf_addr, data);
+    }
 
-char_out = (char) => {
-    let char_h = ((char & 0xF0) + 0x09);
-    let char_l = (((char & 0x0F) << 4) + 0x09);
+    sendCommand (cmd) {
+        cmd |= 0x08; this.byte_out(cmd);
+        cmd |= 0x04; this.byte_out(cmd);
+        cmd &= 0xFB; this.byte_out(cmd);
+    }
 
-	pcf8574_byte_out(char_h);
-	char_h |= 0x04;
-	pcf8574_byte_out(char_h);
-	char_h &= 0xF9;
-	pcf8574_byte_out(char_h);
+    init (i2c_module, addr) {
+        const initString = [0x30, 0x30, 0x30, 0x20, 0x20, 0x80, 0x00, 0x80, 0x00, 0x10, 0x00, 0x60, 0x00, 0xC0];
+        initString.forEach(cmd => this.sendCommand(cmd));
+    }
 
-	pcf8574_byte_out(char_l);
-	char_l |= 0x04;
-	pcf8574_byte_out(char_l);
-	char_l &= 0xF9;
-	pcf8574_byte_out(char_l);
-}
+    char_out (char) {
+        let char_h = ((char & 0xF0) + 0x09);
+        let char_l = (((char & 0x0F) << 4) + 0x09);
 
-print_line = (line) => {
-    for (let i = 0; i < line.length; i++)
-        char_out(line.charCodeAt(i));
-}
+        this.byte_out(char_h);
+        char_h |= 0x04;
+        this.byte_out(char_h);
+        char_h &= 0xF9;
+        this.byte_out(char_h);
 
-print_string = (str) => {
-    const line_length = 16;
-    const cgram_line_gap = line_length + line_length / 2;
+        this.byte_out(char_l);
+        char_l |= 0x04;
+        this.byte_out(char_l);
+        char_l &= 0xF9;
+        this.byte_out(char_l);
+    }
 
-    let line1 = str.slice(0, line_length);
-    let line2 = str.slice(line_length, line_length * 2);
+    print_line (line) {
+        for (let i = 0; i < line.length; i++)
+        this.char_out(line.charCodeAt(i));
+    }
 
-    print_line(line1);
-    if(line2.length) {
-        print_line(Array(cgram_line_gap + 1).join(' '));
-        print_line(line2);
+    print_string (str) {
+        const line_length = 16;
+        const cgram_line_gap = line_length + line_length / 2;
+
+        let line1 = str.slice(0, line_length);
+        let line2 = str.slice(line_length, line_length * 2);
+
+        this.print_line(line1);
+        if(line2.length) {
+            this.print_line(Array(cgram_line_gap + 1).join(' '));
+            this.print_line(line2);
+        }
+    }
+
+    get degree_sign() {
+        return 'ß';
     }
 }
-
-module.exports = {
-    degree_sign: 'ß',
-    init,
-    print_string
-};
